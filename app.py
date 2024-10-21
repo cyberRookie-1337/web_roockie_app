1716a9d28badb4589b50838fbbd5171229293d05
Dimon <nomid@sas-bot-bombom.ru-central1.internal>
2024-10-21 14:16:49 +0000

    delete secret

--- app.py
+++ app.py
@@ -7,10 +7,5 @@ ppath = "./"
 
 AutoIndex(app, browse_root=ppath)
 
-@app.route('/flag')
-def random_fact():
-    fact = random.choice(space_facts)
-    return jsonify({'flag': 'SAS{g1t_folder_is_s0_sw33t'})
-
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=34678)
